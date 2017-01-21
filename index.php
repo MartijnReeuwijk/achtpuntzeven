28838f439d79d41be28f039d5ebfbd980102dfd7
Martijn <m.reeuwijk@live.nl>
2017-01-21 17:05:48 +0100

    nog meer

--- index.php
+++ index.php
@@ -10,12 +10,23 @@
     <meta name="robots" content="index, follow" />
     <meta name="revisit-after" content="3 month" />
 
-    <link rel="apple-touch-icon" sizes="180x180" href="fav/apple-touch-icon.png">
-    <link rel="icon" type="image/png" href="fav/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="fav/favicon-16x16.png" sizes="16x16">
-    <link rel="manifest" href="fav/manifest.json">
-    <link rel="mask-icon" href="fav/safari-pinned-tab.svg">
-    <meta name="theme-color" content="#ffffff">
+    <link rel="apple-touch-icon" sizes="57x57" href="image/fav/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="image/fav/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="image/fav/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="image/fav/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="image/fav/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="image/fav/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="image/fav/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="image/fav/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="image/fav/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="image/fav/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="image/fav/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="image/fav/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="image/fav/favicon-16x16.png">
+    <link rel="manifest" href="image/manifest.json">
+    <meta name="msapplication-TileColor" content="#EAEFF3">
+    <meta name="msapplication-TileImage" content="image/ms-icon-144x144.png">
+    <meta name="theme-color" content="#EAEFF3">
 
     <!-- javascript -->
     <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
--- page/home.php
+++ page/home.php
@@ -1,5 +1,11 @@
 <link rel="stylesheet" href="css/home.css" media="screen" title="no title">
 
+<section class="mantekst">
+<p>
+	Kies een spiergroep.
+</p>
+</section>
+
 <section class="man">
 
 </section>
